572953daf20c4592b6eb5e92f844fe9d128d4b62
Angry_Cookie <fanby2014@126.com>
2023-12-15 15:31:19 +0800

    gitignore

--- workergpt-Backend/LLM_model/Chain.py
+++ workergpt-Backend/LLM_model/Chain.py
@@ -1,22 +1,18 @@
 import zhipuai
 
 from . import zhipuLLM 
+from . import Retrieval
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.output_parser import StrOutputParser
 
-def text_stream(template : str, query : str):
+def text_stream(template : str, query : dict, db : Retrieval.VectorDB, topk: int = 10):
 	prompt = ChatPromptTemplate.from_template(template)
-	test_prompt =ChatPromptTemplate.from_template("hello world!")
 	model = zhipuLLM.ChatGLM()
-	# model = CustomLLM(n=10)
 	output_parser = StrOutputParser()
-	chain1 = prompt | test_prompt
+	keys = list(range(1, len(topk)))
+	values = db.query(query)["documents"]
+	query.update({key: value for key, value in zip(keys, values)})
 	chain = prompt | model | output_parser
 
-	for chunk in chain1.stream(query):
-		print(chunk, end="", flush=True)
 	for chunk in chain.stream(query):
-	
 		print(chunk, end="", flush=True)
-	# res = chain.invoke({"topic": "ice cream"})
-	# print(res)
